ae189cc5da21ef283de3145db81fa62f049eaeb7
Egriet Tasfiyatul Q.j <88014504+egrietqalbi@users.noreply.github.com>
2023-07-25 14:53:41 +0700

    Add files via upload

---
+++ src/pages/data.js
@@ -0,0 +1,9 @@
+export const dataS = [
+    { status : true,
+      nama : "Canon Test",
+      harga : "Rp. 200.000",
+      deskripsi : "tes juga",
+      kelengkapan :["box", "cas ada", "lensa lengkap"],
+      gambar: "hero1.jpg"
+    },
+  ]
